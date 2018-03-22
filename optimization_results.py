2f41561bd8ca44c693e5994f7f68a1edc1a82361
caseynbrock <casey.brock@vanderbilt.edu>
2018-03-22 14:30:06 -0600

    Fix license

--- optimization_results.py
+++ optimization_results.py
@@ -15,7 +15,7 @@
 # 
 # The MIT License
 # 
-# Copyright (c) 2011 Dominic Tarr
+# Copyright (c) 2017 caseynbrock
 # 
 # Permission is hereby granted, free of charge, 
 # to any person obtaining a copy of this software and 
